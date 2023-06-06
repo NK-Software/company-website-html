84a98f2654063a63f9975a372951baf8229ce516
Harry Nguyen <truongnv169@gmail.com>
2023-06-06 14:36:14 +0700

    Update: Contact page

--- assets/js/ajax-form.js
+++ assets/js/ajax-form.js
@@ -32,16 +32,25 @@ $(function() {
 			$('#contact-form input,#contact-form textarea').val('');
 		})
 		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+			// // Make sure that the formMessages div has the 'error' class.
+			// $(formMessages).removeClass('success');
+			// $(formMessages).addClass('error');
+			//
+			// // Set the message text.
+			// if (data.responseText !== '') {
+			// 	$(formMessages).text(data.responseText);
+			// } else {
+			// 	$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			// }
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
 
 			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+			$(formMessages).text('Thank you for your submit ! We will be getting back soon !');
+
+			// Clear the form.
+			$('#contact-form input,#contact-form textarea').val('');
 		});
 	});
 
